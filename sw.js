f0bd03f87fab9d0bbb3801e90ebba84b9764f2a0
mudarismustafa-dotcom <mudarismustafa@gmail.com>
2026-02-26 19:21:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,7 +28,6 @@ self.addEventListener("fetch", (event) => {
   const req = event.request;
   event.respondWith(
     caches.match(req).then((cached) => cached || fetch(req).then((res) => {
-      // cache GET only
       if(req.method === "GET" && res && res.status === 200){
         const copy = res.clone();
         caches.open(CACHE).then(c => c.put(req, copy));
